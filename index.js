e5c9812f560d076260eaa1c5b89ffcca1723ccf7
Braydon Harris <braydonharris@gmail.com>
2021-06-01 13:30:56 -0600

    Code clean up and refactor use yargs instead of minimist Add additional examples to json update readme

--- index.js
+++ index.js
@@ -4,106 +4,62 @@ const AWS = require("aws-sdk");
 AWS.config.update({region: 'us-east-1'});
 const ssm = new AWS.SSM();
 const obj = require("./params.json");
-const args = require('minimist')(process.argv.slice(2));
+const args = require('yargs/yargs')(process.argv.slice(2))
+.array('name').array('stage').argv;
 
-let names;
-let stages;
-let promises = [];
-
-// no args deploy all
-if(!args['name'] && !args['stage']) {
-	// deploy all
-	deployAll()
+const prepDeploy = (names, stages) => {
+	let results = [];
+	let params = filterObject(names, stages);
+	params.forEach(x => {
+		results.push(ssm.putParameter(x));
+	});
+	return results;
 }
 
-// has names but no stages
-if(args['name'] && !args['stage']) {
-	deploySomeName();
-}
-
-// has no name but has stages
-if(!args['name'] && args['stage']) {
-	deploySomeStage();
-}
-
-// has names and stages
-if(args['name'] && args['stage']) {
-	deploySome()
-}
-
-function deployAll() {
-	for (let i = 0; i < obj.length; i++) {
-		const el = obj[i];
-		for (const key in el) {
-			if(Array.isArray(el[key]) && el[key].length !== 0) {
-				for (let j = 0; j < el[key].length; j++) {
-					const child = el[key][j];
-					promises.push(ssm.putParameter(child));
+const filterObject = (names, stages) => {
+	let parent = [];
+	if (!names) {parent = obj}
+	// filter the names
+	names.forEach(x => {
+		const params = obj.filter(param => param.name === x);
+		if(!params.length) {
+			console.warn('No params found by name:', x);
+			return;
+		};
+		parent.push(params[0])
+	});
+	let arr = []
+	if(!stages){
+		// build array of params
+		parent.forEach(y => {
+			for (const key in y) {
+				if(Array.isArray(y[key])) {
+					arr = arr.concat(y[key]);
 				}
 			}
-		}
+		});
+		return arr;
 	}
-}
-
-function deploySomeName() {
-	names = args['name'];
-	if(!args['name'].isArray) { names = [args['name']] }
-	for (let i = 0; i < names.length; i++) {
-		const el = names[i];
-		const params = obj.filter(param => param.name === el);
-		if(!params.length) return;
-		for (const key in params[0]) {
-			if(Array.isArray(params[0][key]) && params[0][key].length !== 0) {
-				for (let j = 0; j < params[0][key].length; j++) {
-					const child = params[0][key][j];
-					promises.push(ssm.putParameter(child));
-				}
+	//filter the stages
+	stages.forEach(x => {
+		parent.forEach(y => {
+			if(y[x] === undefined || !y[x].length) {
+				console.warn('No prams found by stage: ', x)
+				return;
 			}
-		}
-	}
+			arr = arr.concat(y[x]);
+		})
+	});
+	return arr;
 }
 
-function deploySomeStage() {
-	stages = args['stage'];
-	if(!args['stage'].isArray) { stages = [args['stage']] }
-	 
-	for (let i = 0; i < obj.length; i++) {
-		const el = obj[i];
-		for (let j = 0; j < stages.length; j++) {
-			const s = stages[j];
-			const env = el[s];
-			if(!env || !env.length) return;
-			for (let k = 0; k < env.length; k++) {
-				promises.push(ssm.putParameter(env[k]));		
-			}
-		}
+async function doAllSequentually(arrFnPromiseList) {
+	if(!arrFnPromiseList || !Array.isArray(arrFnPromiseList)) { 
+		spinner.fail();
+		console.warn('Promise list doesnt exist or isnt an array.')
+		return; 
 	}
+	arrFnPromiseList.forEach(async (x)=> {await x.promise();});
 }
 
-function deploySome() {
-	names = args['name'];
-	stages = args['stage'];
-	if(!args['name'].isArray) { names = [args['name']] }
-	if(!args['stage'].isArray) { stages = [args['stage']] }
-	
-	for (let i = 0; i < names.length; i++) {
-		const el = names[i];
-		const params = obj.filter(param => param.name === el);
-		if(!params.length) return;
-		for (let i = 0; i < stages.length; i++) {
-			const el = stages[i];
-			const env = params[0][el];
-			if(!env || !env.length) return;
-			for (let j = 0; j < env.length; j++) {
-				promises.push(ssm.putParameter(env[j]));		
-			}
-		}
-	}
-}
-
-async function doAllSequentually(fnPromiseArr) {
-	for (let i=0; i < fnPromiseArr.length; i++) {
-		const val = await fnPromiseArr[i].promise();
-	}
-}
-doAllSequentually(promises).then(() => spinner.succeed());
+doAllSequentually(prepDeploy(args.name, args.stage)).catch(err => { console.warn(err)}).then(() => spinner.succeed());
